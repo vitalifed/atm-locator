7dde43ed5234bd7411eb5c8f9491a3d7e0cef5f0
Vitali Fedasenka <vfedasenka@marktplaats.nl>
2020-02-15 20:45:54 +0100

    Upgrade log4 to 2.x

--- src/main/java/com/backbase/assignment/solution/controller/AtmLocatorController.java
+++ src/main/java/com/backbase/assignment/solution/controller/AtmLocatorController.java
@@ -1,58 +1,53 @@
 package com.backbase.assignment.solution.controller;
 
-import org.apache.log4j.Logger;
+import com.backbase.assignment.solution.config.AbstractUserController;
+import com.backbase.assignment.solution.flow.Flow;
+import com.backbase.assignment.solution.service.AtmRestClient;
+import com.backbase.assignment.solution.service.ProcessorFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.backbase.assignment.solution.config.AbstractUserController;
-import com.backbase.assignment.solution.flow.Flow;
-import com.backbase.assignment.solution.service.AtmRestClient;
-import com.backbase.assignment.solution.service.ProcessorFactory;
-import com.backbase.assignment.solution.util.Processor;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(path="atm")
+@RequestMapping(path = "atm")
 public class AtmLocatorController extends AbstractUserController {
-	
-	private static final Logger log = Logger.getLogger(AtmLocatorController.class);
-	
-	@Autowired
-	private AtmRestClient atmRestClient; 
-	
-	@Autowired
-	private ProcessorFactory processorFactory; 
+
+    private static final Logger log = LogManager.getLogger(AtmLocatorController.class);
+
+    @Autowired
+    private AtmRestClient atmRestClient;
+
+    @Autowired
+    private ProcessorFactory processorFactory;
 
     @RequestMapping(path = "/{bank}/raw", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
     @ResponseBody
-    public ResponseEntity<String> getAllLoacationsInRawFormat(@PathVariable("bank") String bank, @RequestParam(name="city", required=false) String city){
-    	final HttpHeaders httpHeaders= new HttpHeaders();
-    	httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-    	
-    	log.info("User="+ getPrincipal()+", searching ATM by city="+city);
-    	
-    	return new ResponseEntity<String>(processorFactory.processor(Flow.valueOf(bank.toUpperCase()+"R").getProcessor()).filterByCity(atmRestClient.getAllLocations(), city), httpHeaders, HttpStatus.OK);
-    }
-	
-	@RequestMapping(path = "/{bank}/search", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
-	@ResponseBody
-	public ResponseEntity<String> searchByCity(@PathVariable("bank") String bank, @RequestParam(name="city", required=false) String city){
-    	final HttpHeaders httpHeaders= new HttpHeaders();
+    public ResponseEntity<String> getAllLoacationsInRawFormat(@PathVariable("bank") String bank, @RequestParam(name = "city", required = false) String city) {
+        final HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        
-        log.info("User="+ getPrincipal()+", searching ATM by city="+city);
-        
+
+        log.info("User=" + getPrincipal() + ", searching ATM by city=" + city);
+
+        return new ResponseEntity<String>(processorFactory.processor(Flow.valueOf(bank.toUpperCase() + "R").getProcessor()).filterByCity(atmRestClient.getAllLocations(), city), httpHeaders, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/{bank}/search", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public ResponseEntity<String> searchByCity(@PathVariable("bank") String bank, @RequestParam(name = "city", required = false) String city) {
+        final HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        log.info("User=" + getPrincipal() + ", searching ATM by city=" + city);
+
         String value = atmRestClient.getAllLocations();
-		return new ResponseEntity<String>(processorFactory.processor(Flow.valueOf(bank.toUpperCase()).getProcessor()).filterByCity(value, city), httpHeaders, HttpStatus.OK);
-	}
-	
+        return new ResponseEntity<String>(processorFactory.processor(Flow.valueOf(bank.toUpperCase()).getProcessor()).filterByCity(value, city), httpHeaders, HttpStatus.OK);
+    }
+
 
 }
--- src/main/java/com/backbase/assignment/solution/util/JsonResponseProcessor.java
+++ src/main/java/com/backbase/assignment/solution/util/JsonResponseProcessor.java
@@ -1,49 +1,46 @@
 package com.backbase.assignment.solution.util;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import org.apache.log4j.Logger;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class JsonResponseProcessor extends AbstractResponseProcessor {
 
-	private static final Logger log = Logger.getLogger(JsonResponseProcessor.class);
+    private static final Logger log = LogManager.getLogger(JsonResponseProcessor.class);
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public String filterByCity(String inputJSON, String city) {
-		try {
-			List<Map> locations = readValue(inputJSON);
-			Iterator<Map> it = locations.iterator();
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public String filterByCity(String inputJSON, String city) {
+        try {
+            List<Map> locations = readValue(inputJSON);
+            Iterator<Map> it = locations.iterator();
 
-			List resultList = new ArrayList<>();
+            List resultList = new ArrayList<>();
 
-			while (it.hasNext()) {
-				Map<String, Map> address = it.next();
-				if (city != null && city.length() > 0
-						&& !city.trim().equalsIgnoreCase((String) address.get("address").get("city"))) {
-					it.remove();
-					continue;
-				}
-				// Remove useless data
-				resultList.add(address.get("address").values().stream().filter(obj -> String.class.isInstance(obj))
-						.collect(Collectors.toList()));
-			}
-			Map<String, List> results = new HashMap<>();
-			results.put("data", resultList);
+            while (it.hasNext()) {
+                Map<String, Map> address = it.next();
+                if (city != null && city.length() > 0
+                        && !city.trim().equalsIgnoreCase((String) address.get("address").get("city"))) {
+                    it.remove();
+                    continue;
+                }
+                // Remove useless data
+                resultList.add(address.get("address").values().stream().filter(String.class::isInstance)
+                        .collect(Collectors.toList()));
+            }
+            Map<String, List> results = new HashMap<>();
+            results.put("data", resultList);
 
-			return writeValueAsString(results);
+            return writeValueAsString(results);
 
-		} catch (IOException e) {
-			// Just ignore
-			log.error(e.getMessage(), e);
-		}
+        } catch (IOException e) {
+            // Just ignore
+            log.error(e.getMessage(), e);
+        }
 
-		return "";
-	}
+        return "";
+    }
 
 }
